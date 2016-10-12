7768f07acbe57b45c7f59d4b6b0d5b248ba16f18
Rene-Jacques Riel <rie@qlik.com>
2016-10-11 23:12:48 -0400

    updating the client to run properly with the new api/socket format

--- README.md
+++ README.md
@@ -14,10 +14,12 @@ To get up and running please follow the following steps
 3. add a config.js file to the root of the project. It should look like the following:
 ```
     module.exports = {
-      host: <The WebSocket to connect to (http://server:port)>
+      host: <The server to connect to (ie. localhost)>,
+      socketPort: <The port to connect to the socket over (ie. 3000)>,
+      apiPort: <The port to connect to the api over (ie. 8080)>
     }
 ```
-4. run `node index.js <id of player to connect as>`
+4. run `node index.js <player api id> <player api secret>`
 
 ## Contributing
 Please feel free to contribute to this project. Work on issues, open issues for bugs/enhancements and create pull requests. You can view the [CONTRIBUTING](CONTRIBUTING) document for more information
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const config = require('./config')
 const io = require('socket.io-client')
+const request = require('request')
 let lastMove
+let playerId
 
 console.log('connecting')
-let socket = io.connect(config.host)
+
+let queryData = `apiId=${process.argv[2]}&apiSecret=${process.argv[3]}`
+let apiKey = new Buffer(`${process.argv[2]}:${process.argv[3]}`).toString('base64')
+
+let socket = io.connect(`http://${config.host}:${config.socketPort}`, {query: queryData})
 console.log('connection requested')
 
-var keypress = require('keypress');
+var keypress = require('keypress')
 
-keypress(process.stdin);
+keypress(process.stdin)
 
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
@@ -17,74 +23,129 @@ process.stdin.on('keypress', function (ch, key) {
   }
   if (key && key.name == 'a') {
     console.log('attack')
-    socket.emit('attack')
+    lastMove = 'heal'
+    performMove()
   }
   if (key && key.name == 'h') {
     console.log('heal')
-    socket.emit('heal')
+    lastMove = 'attack'
+    performMove()
   }
-});
+})
 
-process.stdin.resume();
+process.stdin.resume()
 
 socket.on('connect', (data) => {
 
+  let playerId
   console.log('connected')
   // once connected we need to enter the arena
   // passing in our ID
-  socket.emit('enter arena',{id: process.argv[2]})
 
-
-  socket.on('in arena',() => {
-    console.log('in the arena')
-
-    // when in the arena, we want to let the server know we are ready to play
-    socket.emit('ready to play',{})
+  socket.on('success', (player) => {
+    playerId = player.id
+    console.log('logged in')
   })
 
 
   socket.on('start game', (game) => {
 
     console.log('game started')
-
     // if the current player is us, we want to play
-    if (game.current === process.argv[2]) {
-      console.log('your turn')
-    }
+    getGame(game.id)
+        .then((game) => {
+          if (game.current === playerId) {
+            console.log("your turn. type 'a <enter>' to attack or 'h <enter>' to heal.")
+          }
+        })
+  })
+
+  socket.on('in game', (game) => {
+
+    console.log('already in game')
+
+    getGame(game.id)
+        .then((game) => {
+          if (game.current === playerId) {
+            console.log('your turn')
+          }
+        })
   })
 
 
   socket.on('move played', (move) => {
-    console.log("MOVE PLAYED", move)
-
     // if the move just played wasn't by us, we want to
     // make a move
-    if(move.player != process.argv[2]) {
+
+    if (move.player != playerId) {
+      console.log(`opponent performed ${move.result}`)
       performMove()
     }
   })
 
   socket.on('invalid', (error) => {
     // an invalid action has occurred, we should log it
-    console.log("INVALID ACTION", error)
+    console.log("Invalid Action", error)
   })
 
   socket.on('game over', (game) => {
     // game is over
-    console.log("GAME OVER", game)
+    console.log("Game Over")
+    if(game.game.winner === playerId) {
+      console.log("You Won!!!")
+    } else {
+      console.log("You Lost :(")
+    }
   })
 })
+
+let getGame = (gameId) => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      uri: `http://${config.host}:${config.apiPort}/games/${gameId}`,
+      method: 'GET',
+      headers: {
+        "Authorization": `Basic ${apiKey}`
+      }
+    }
+
+    request.get(options, (error, res, body) => {
+      if (error) {
+        console.error("Error Getting Game", error)
+        reject(error)
+      } else {
+        resolve(JSON.parse(body))
+      }
+    })
+  })
+}
 
 let performMove = () => {
 
   // this is where we would put our logic for deciding which move to make
   // here we are just doing the opposite of what we did last
 
-  if (lastMove !== "attack") {
-    lastMove = "attack"
-    socket.emit("attack")
-  } else {
-    lastMove = "heal"
-    socket.emit("heal")
+  lastMove = lastMove === "attack" ? "heal" : "attack"
+  var body = {action: lastMove}
+
+  const options = {
+    uri: `http://${config.host}:${config.apiPort}/moves`,
+    method: 'POST',
+    headers: {
+      "Authorization": `Basic ${apiKey}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
   }
+
+  request.post(options, (error, res, body) => {
+    if(error) {
+      console.log("Error Performing Move", error)
+    } else if (res.statusCode != 200) {
+      console.log("Error Performing Move", res.body)
+    } else {
+      console.log(`${lastMove} performed successfully`)
+    }
+  })
+
 }
